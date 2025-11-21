5266ab9596bca47befb718093263d9720c49dd6f
bassboy7 <zkhaywa@gmail.com>
2025-11-21 15:59:20 +0000

    yeah

--- NOSTRUM/Assets/Alpha Game Test1/Game Test 1/Scripts/Player Movement.cs
+++ NOSTRUM/Assets/Alpha Game Test1/Game Test 1/Scripts/Player Movement.cs
@@ -1,5 +1,6 @@
 using System;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class NewMonoBehaviourScript : MonoBehaviour
 {
@@ -8,12 +9,14 @@ public class NewMonoBehaviourScript : MonoBehaviour
     public Transform groundCheck;
     public float groundCheckRadius = 0.2f;
     public LayerMask groundLayer;
-    
+
     private SpriteRenderer spriteRenderer;
-    
+
     private Rigidbody2D rb;
     private bool isGrounded;
-    
+
+    private int numOfEnemies = 10;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -32,26 +35,65 @@ public class NewMonoBehaviourScript : MonoBehaviour
         float moveInput = Input.GetAxis("Horizontal");
         rb.linearVelocity = new Vector2(moveInput * moveSpeed, rb.linearVelocity.y);
 
-        if (Input.GetKeyDown(KeyCode.Space) &&  isGrounded)
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+
         {
             rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
         }
-        
+
+
         if (Input.GetKeyDown(KeyCode.Joystick1Button1) && isGrounded)
+
         {
             rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
         }
+
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            if (rb.linearVelocity.y > 0)
+            {
+                rb.linearVelocity = new Vector2(rb.linearVelocity.x, rb.linearVelocity.y * 0.5f);
+            }
+        }
+
+        if (Input.GetKeyUp(KeyCode.Joystick1Button1))
+        {
+            if (rb.linearVelocity.y > 0)
+            {
+                rb.linearVelocity = new Vector2(rb.linearVelocity.x, rb.linearVelocity.y * 0.5f);
+            }
+        }
+
+        if (Input.GetKeyUp(KeyCode.B))
+        {
+            numOfEnemies -= 1;
+            Debug.Log("Enemies left: " + numOfEnemies);
+            if (numOfEnemies == 0)
+            {
+                Debug.Log("Enemies over");
+                Invoke("NextScene", 3f);
+            }
+
+        }
+
+
+
+    }
+
+    void NextScene()
+    {
+        SceneManager.LoadScene("Alpha Game Test1/Game Test 1/Scenes/TutorialRoom");
     }
 
     private void FixedUpdate()
     {
-        
+
         if (Input.GetAxisRaw("Horizontal") > 0)
             spriteRenderer.flipX = false;
         else if (Input.GetAxisRaw("Horizontal") < 0)
             spriteRenderer.flipX = true;
-        
-        isGrounded =  Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
+
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
     }
 }
 
