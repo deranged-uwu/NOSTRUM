17f27d02f5235c608b873129c25cede33ae7b2ff
bassboy7 <zkhaywa@gmail.com>
2025-12-01 20:56:14 +0000

    patrol

--- NOSTRUM/Assets/Alpha Game Test1/Game Test 1/Scripts/Enemy.cs
+++ NOSTRUM/Assets/Alpha Game Test1/Game Test 1/Scripts/Enemy.cs
@@ -1,3 +1,4 @@
+using System.IO;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
@@ -5,6 +6,22 @@ public class Enemy : MonoBehaviour
     public Animator anim;
     public MonsterHealth monsterHealth;
 
+    public float speed = 2.5f;
+    private float direction = 1f;
+
+    private void Update()
+    {
+        transform.Translate(speed * direction * Time.deltaTime, 0, 0);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.CompareTag("Barrier"))
+        {
+            direction *= -1;
+            transform.localScale = new Vector3(direction,1,1);
+        }
+    }
 
     private void OnEnable()
     {
