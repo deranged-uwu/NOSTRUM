386774ab97e9f0e5cb69a9c3776373e4a46584a1
bassboy7 <zkhaywa@gmail.com>
2025-12-18 14:42:01 +0000

    QOL

--- NOSTRUM/Assets/Alpha Game Test1/Game Test 1/Scripts/UI Menu/Main Menu/MainMenu.cs
+++ NOSTRUM/Assets/Alpha Game Test1/Game Test 1/Scripts/UI Menu/Main Menu/MainMenu.cs
@@ -5,7 +5,7 @@ public class MainMenu : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     public void StartGame()
     {
-        SceneManager.LoadScene("Alpha Game Test1/Game Test 1/Scenes/TutorialRoom");
+        SceneManager.LoadScene("Alpha Game Test1/Game Test 1/Scenes/Levels/TutorialRoom");
     }
 
     // Update is called once per frame
